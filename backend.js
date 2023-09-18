5050210bd4182c45e70cef6fcbfbffda2b50ee60
Moacir Roger <100392409+Roger-snts@users.noreply.github.com>
2023-09-18 19:17:58 -0400

    Menu em todas as páginas

---
+++ backend.js
@@ -0,0 +1,11 @@
+
+let menu = document.getElementById("imenuItens")
+
+function exibirMenu() {
+    if (menu.style.display == "none") {
+        menu.style.display = block;
+    }
+    else {
+        menu.style.display = none;
+    }
+}
