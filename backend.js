5d4911f4ad701f03d96c5b05fda0bba9e11f9e7d
Moacir Roger <100392409+CoffeNoMilk@users.noreply.github.com>
2023-09-25 18:08:31 -0300

    Menu pronto

--- backend.js
+++ backend.js
@@ -1,11 +1,10 @@
 
-let menu = document.getElementById("imenuItens")
+let menu = document.getElementById("iMenuItens")
 
 function exibirMenu() {
     if (menu.style.display == "none") {
-        menu.style.display = block;
-    }
-    else {
-        menu.style.display = none;
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
     }
 }
