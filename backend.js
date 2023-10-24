babd75b9f36222d8ead18661d38d70ded0c4fc66
Moacir Roger <100392409+Roger-snts@users.noreply.github.com>
2023-10-23 21:42:14 -0400

    Mudanças
    Mudei o menu o tornando responsivo. Agora ele pode ser alterado de acordo com o tamanho da tela.

--- backend.js
+++ backend.js
@@ -1,6 +1,14 @@
 
 let menu = document.getElementById("iMenuItens")
 
+function alterarTamanho(){
+    if (window.innerWidth >= 720){
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
+
 function exibirMenu() {
     if (menu.style.display == "none") {
         menu.style.display = "block";
